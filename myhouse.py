249ef901a4eb76d7d5a7952422bc587e6fb85b1e
Timofey Khirianov <tkhirianov@gmail.com>
2019-11-25 17:54:17 +0300

    Реализовал функции рисования основания и крыши.

--- myhouse.py
+++ myhouse.py
@@ -3,7 +3,7 @@ from graph import *
 
 def paint_house(x, y, width, height):
     foundation_height = int(height * 0.1)
-    walls_height = int(height * 0.5)
+    walls_height = int(height * 0.6)
     roof_height = height - foundation_height - walls_height
 
     paint_foundation(x, y + walls_height, width, foundation_height)
@@ -12,18 +12,21 @@ def paint_house(x, y, width, height):
 
 
 def paint_foundation(x, y, width, height):
-    print(1)
-    pass
+    brushColor(80, 50, 0)
+    penColor("black")
+    rectangle(x, y, x + width, y + height)
 
 
 def paint_walls(x, y, width, height):
-    print(2)
-    pass
+    brushColor(180, 40, 0)
+    penColor("black")
+    rectangle(x, y, x + width, y + height)
 
 
-def paint_roof(x, y, widht, height):
-    print(3)
-    pass
+def paint_roof(x, y, width, height):
+    brushColor(180, 190, 30)
+    penColor("black")
+    polygon([(x, y), (x + width // 2, y - height), (x + width, y)])
 
 
 
