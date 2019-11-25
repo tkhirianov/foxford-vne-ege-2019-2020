d379bb2880d674eeb46f1256027facc13a65bdfd
Timofey Khirianov <tkhirianov@gmail.com>
2019-11-25 17:25:14 +0300

    Размечтался о строительстве-рисовании дома.

---
+++ myhouse.py
@@ -0,0 +1,16 @@
+from graph import *
+
+
+def paint_house(x, y, width, height):
+    pass
+    print("DEBUG: paint_house() called with parameters:",
+          x, y, width, height)
+
+
+def main():
+    paint_house(150, 150, 120, 100)
+    run()
+
+
+
+main()
