d6b79a0b2e0e972a72a42909fc8e4f0bce8530ff
Timofey Khirianov <tkhirianov@gmail.com>
2019-11-25 17:43:57 +0300

    Реализовал функцию paint_house

--- myhouse.py
+++ myhouse.py
@@ -2,9 +2,30 @@ from graph import *
 
 
 def paint_house(x, y, width, height):
+    foundation_height = int(height * 0.1)
+    walls_height = int(height * 0.5)
+    roof_height = height - foundation_height - walls_height
+
+    paint_foundation(x, y + walls_height, width, foundation_height)
+    paint_walls(x, y, width, walls_height)
+    paint_roof(x, y, width, roof_height)
+
+
+def paint_foundation(x, y, width, height):
+    print(1)
     pass
-    print("DEBUG: paint_house() called with parameters:",
-          x, y, width, height)
+
+
+def paint_walls(x, y, width, height):
+    print(2)
+    pass
+
+
+def paint_roof(x, y, widht, height):
+    print(3)
+    pass
+
+
 
 
 def main():
