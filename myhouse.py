b75f8c00ef9bfe3f7732c1d7b90243f611022142
Timofey Khirianov <tkhirianov@gmail.com>
2019-11-25 18:31:13 +0300

    Анимация домика
    Функция `main()` переименована в `init()`
    Появилась функция tick() на *таймере*.
    Сделал Анимацию домика со стиранием _всего_ на холсте.

--- myhouse.py
+++ myhouse.py
@@ -1,13 +1,18 @@
 from graph import *
 
+WIDTH = 150
+HEIGHT = 140
+
 
 def paint_house(x, y, width, height):
     foundation_height = int(height * 0.1)
     walls_height = int(height * 0.6)
     roof_height = height - foundation_height - walls_height
-
+    foundation_ledge = int(width * 0.05)
+    walls_width = width - 2*foundation_ledge
+    
     paint_foundation(x, y + walls_height, width, foundation_height)
-    paint_walls(x, y, width, walls_height)
+    paint_walls(x+foundation_ledge, y, walls_width, walls_height)
     paint_roof(x, y, width, roof_height)
 
 
@@ -29,12 +34,29 @@ def paint_roof(x, y, width, height):
     polygon([(x, y), (x + width // 2, y - height), (x + width, y)])
 
 
+def clear_canvas():
+    for item in canvas().find_all():
+        canvas().delete(item)
 
 
-def main():
-    paint_house(150, 150, 120, 100)
+def tick():
+    global x, y, scale
+    x = x + 2
+    y = y - 1
+    scale = scale - 0.3
+    clear_canvas()
+    paint_house(x, y, int(WIDTH*scale/100), int(HEIGHT*scale/100))
+
+
+def init():
+    global x, y, scale
+    x = 150
+    y = 350
+    scale = 100
+    paint_house(x, y, int(WIDTH*scale/100), int(HEIGHT*scale/100))
+
+    onTimer(tick, 50)
     run()
 
 
-
-main()
+init()
