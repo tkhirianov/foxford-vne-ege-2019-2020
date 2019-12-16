0ea2ad5e90ed49d9a4ab075f0dc1b383f25b112a
Timofey Khirianov <tkhirianov@gmail.com>
2019-12-16 17:22:36 +0300

    Флаер выделен как набор функций и переменных

--- lesson_11/flier.py
+++ lesson_11/flier.py
@@ -3,9 +3,29 @@ import tkinter as tk
 WIDTH, HEIGHT = 450, 380
 
 
-def next_frame_job(x):
-    print("frame", x)
-    canvas.after(200, next_frame_job, x+1)
+# ========= Model ==========
+def flier_init():
+    global flier, flier_x, flier_y, flier_width, flier_height
+    flier_x, flier_y = 150, 225
+    flier_width, flier_height = 100, 50
+    flier = canvas.create_rectangle(flier_x,
+                                    flier_y,
+                                    flier_x + flier_width,
+                                    flier_y + flier_height,
+                                    fill="blue")
+
+def flier_move():
+    global flier_x
+    canvas.coords(flier, flier_x, flier_y,
+                  flier_x + flier_width, flier_y + flier_height)
+    flier_x += 1
+
+
+# ======== Control and View ========
+def next_frame_job(n):
+    print("frame", n)
+    flier_move()
+    canvas.after(20, next_frame_job, n+1)
 
 
 def initilization():
@@ -15,10 +35,8 @@ def initilization():
     canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH,
                        background="lightblue")
     canvas.pack()
-    flier = canvas.create_rectangle(50, 25, 150, 75,
-                                    fill="blue")
-    canvas.after(1000, next_frame_job, 1)
-
+    flier_init()
+    canvas.after(2000, next_frame_job, 1)
 
 def main():
     initilization()
