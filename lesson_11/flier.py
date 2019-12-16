c75ddaa014b3b21153d9627c22a74b35c06b384e
Timofey Khirianov <tkhirianov@gmail.com>
2019-12-16 16:42:16 +0300

    Добавил обработчик события, запускаемый по времени.

--- lesson_11/flier.py
+++ lesson_11/flier.py
@@ -1,12 +1,23 @@
 import tkinter as tk
 
-WIDTH, HEIGHT = 500, 380
+WIDTH, HEIGHT = 450, 380
+
+
+def next_frame_job(x):
+    print("frame", x)
+    canvas.after(200, next_frame_job, x+1)
 
 
 def initilization():
     global root, canvas
     root = tk.Tk()
     root.geometry(f"{WIDTH}x{HEIGHT}")
+    canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH,
+                       background="lightblue")
+    canvas.pack()
+    flier = canvas.create_rectangle(50, 25, 150, 75,
+                                    fill="blue")
+    canvas.after(1000, next_frame_job, 1)
 
 
 def main():
