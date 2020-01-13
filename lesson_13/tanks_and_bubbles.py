c92ef3e914216b13a8ba2e664df6e4c55383a802
Timofey Khirianov <tkhirianov@gmail.com>
2020-01-13 17:47:27 +0300

    Реализовал класс главного окна.
    Кнопка запуска игры создаёт холст и объект игрового раунда.
    Также привязываются обработчики - циклический по времени и по клику мышки.

--- lesson_13/tanks_and_bubbles.py
+++ lesson_13/tanks_and_bubbles.py
@@ -4,7 +4,8 @@ import tkinter as tk
 WIDTH, HEIGHT = 450, 380
 DT = 0.2
 GRAVITY_CONSTANT = 3
-
+START_PAUSE = 1000
+FRAME_TIME = 20  #microseconds
 
 # ========= Model ==========
 
@@ -34,18 +35,19 @@ class Bubble:
 class GameRound:
     """ Игровой раунд.
         Здесь в атрибутах содержатся ссылки на:
-        1. объект танка
-        2. список текущих, "живых" снарядов
-        3. список пузыриков
+        1. холст
+        2. танк
+        3. список текущих, "живых" снарядов
+        4. список пузыриков
     """    
-    def __init__(self):
+    def __init__(self, canvas):
         pass
 
     def handle_frame(self):
-        pass
+        print('handled frame')
 
     def handle_click(self, event):
-        pass
+        print('handled click')
 
 # ======== Control and View ========
 
@@ -53,23 +55,42 @@ class MainWindow:
     """ Главное окно.
         Содержит:
         1. ссылку на root = Tk()
-        2. ссылки на все необходимые виджеты: кнопку, лэйбл с очками и т.п.
-        3. ссылку на экземпляр игрового раунда.
+        2. ссылку на экземпляр игрового раунда.
+        3*. ссылки на все необходимые виджеты: кнопку, лэйбл с очками и т.п.
     """
     def __init__(self):
-        pass
+        self._root = tk.Tk()
+        self._game = None
+        self._restart_button = tk.Button(self._root, text="Перезапустить игру",
+                                         command=self._restart_button_handler)
+        self._restart_button.pack()
 
     def start_game(self):
-        pass
+        canvas = tk.Canvas(self._root, height=HEIGHT, width=WIDTH,
+                           background="lightblue", border=3)
+        canvas.pack()
+        self._game = GameRound(canvas)
+        canvas.bind("<Button-1>", self._handle_click)
+        canvas.after(START_PAUSE, self._handle_frame)
 
     def _handle_frame(self):
-        pass
+        if self._game is not None:
+            self._game.handle_frame()
+        self._root.after(FRAME_TIME, self._handle_frame)
 
     def _handle_click(self, event):
-        pass
+        if self._game is not None:
+            self._game.handle_click(event)
+
+    def _restart_button_handler(self):
+        if self._game is None:
+            self.start_game()
+            print("Запустили игру...")
+        else:
+            print("Игра уже запущена!")
 
     def mainloop(self):
-        pass
+        self._root.mainloop()
 
 
 def main():
